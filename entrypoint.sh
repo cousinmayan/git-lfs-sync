16177438f035afa6466de957142ad8cf7b0e6f14
Cody Estes <cody@dreamfan.com>
2019-04-24 08:07:37 -0500

    wip

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -l
+
+# Authenticate Git LFS to avoid rate limiting
+echo "machine github.com
+        login ${GITHUB_TOKEN}" > ${HOME}/.netrc
+git config lfs.https://github.com/${GITHUB_REPOSITORY}.git/info/lfs.access basic
+
+echo "Git LFS sees these files..."
+git lfs ls-files
+
+echo "Git LFS fetch..."
+git lfs fetch --all
+
+echo "Git LFS checkout..."
+git lfs checkout
+
+echo "Git LFS finished."
