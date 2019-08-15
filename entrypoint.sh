466e43c712cd3726cfc609ad3f889e11f3a3bab9
Cody Estes <cody@dreamfan.com>
2019-08-15 11:55:12 -0500

    clarify some things

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,10 @@
 set -eu
 
 # Authenticate Git LFS to avoid rate limiting
-echo "machine github.com
-        login ${GITHUB_TOKEN}" > ${HOME}/.netrc
+echo "Adding github login token to netrc..."
+echo "machine github.com login ${GITHUB_TOKEN}" > ${HOME}/.netrc
+
+echo "Using Git config to set LFS access to basic..."
 git config lfs.https://github.com/${GITHUB_REPOSITORY}.git/info/lfs.access basic
 
 echo "Git LFS sees these files..."
