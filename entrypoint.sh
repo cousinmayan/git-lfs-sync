93e2c9e020be57b8226fefb1ff4cda51552ba95b
Cody Estes <cody@dreamfan.com>
2019-08-20 11:32:16 -0500

    simplify to pull

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,10 +12,7 @@ git config lfs.https://github.com/${GITHUB_REPOSITORY}.git/info/lfs.access basic
 echo "Git LFS sees these files..."
 git lfs ls-files
 
-echo "Git LFS fetch..."
-git lfs fetch --all
+echo "Git LFS pull..."
+git lfs pull
 
-echo "Git LFS checkout..."
-git lfs checkout
-
-echo "Git LFS finished."
+echo "Git LFS pull finished."
